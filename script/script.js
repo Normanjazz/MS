d07f76a8fd958f7931f13959bdefe8b5463e0d6e
MoyaShladost <kulavets@yandex.ru>
2021-06-25 14:50:54 +0400

    creating Gallary

--- script/script.js
+++ script/script.js
@@ -6,3 +6,13 @@ for(var i=0; i< cards.length; i++){
     });
 }
 
+
+// создает из div c id = "into" изображения из папки img/gallery/ с классом "image"
+let into = document.getElementById("into"), s = "";
+for (let i = 1; i <= 14; ++i) {
+    s += `<div class="image"><img src="img/gallery/img (${i}).png"></div>`;
+    }
+into.innerHTML = s;
+
+
+//
