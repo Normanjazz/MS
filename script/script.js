c33f8054f75b452d1c2827f04715aa097d629092
MoyaShladost <kulavets@yandex.ru>
2021-07-04 16:24:40 +0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -139,3 +139,16 @@ function Accordion(){
 }
 Accordion()
 //==========================================================================
+
+//Плавная прокрутка страницы
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
