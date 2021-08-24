58a3c559e27baf40577ca0dd5dd421dda953afe2
MoyaShladost <kulavets@yandex.ru>
2021-08-24 22:36:41 +0400

    CalcComments

--- script/calc.js
+++ script/calc.js
@@ -1,76 +1,3 @@
-const productData = [
-    {
-        "id": "prod0",
-        "name": "Выберите продукт",
-        "unit": "",
-        "qty": "",
-        "unitprice": ""
-    },
-    {
-        "id": "prod1",
-        "name": "Масло сливочное",
-        "unit": "гр",
-        "qty": "500",
-        "unitprice": "0.19"
-    },
-    {
-        "id": "prod2",
-        "name": "Молоко",
-        "unit": "мл",
-        "qty": "1000",
-        "unitprice": "0.05"
-    },
-    {
-        "id": "prod3",
-        "name": "Мука",
-        "unit": "гр",
-        "qty": "2000",
-        "unitprice": "0.04"
-    },
-    {
-        "id": "prod4",
-        "name": "Разрыхлитель",
-        "unit": "уп",
-        "qty": "1",
-        "unitprice": "12.00"
-    },
-    {
-        "id": "prod5",
-        "name": "Сахар-песок",
-        "unit": "гр",
-        "qty": "5000",
-        "unitprice": "0.05"
-    },
-    {
-        "id": "prod6",
-        "name": "Сахарная пудра",
-        "unit": "гр",
-        "qty": "1000",
-        "unitprice": "0.12"
-    },
-    {
-        "id": "prod7",
-        "name": "Сливки",
-        "unit": "гр",
-        "qty": "1000",
-        "unitprice": "0.28"
-    },
-    {
-        "id": "prod8",
-        "name": "Сыр творожный",
-        "unit": "гр",
-        "qty": "1000",
-        "unitprice": "0.94"
-    },
-    {
-        "id": "prod9",
-        "name": "Яйца куриные",
-        "unit": "шт",
-        "qty": "10",
-        "unitprice": "7.00"
-    }
-]
-
 //Счетчик для создания и удаления элементов по ID
 var id_number = 1;
 
@@ -83,11 +10,11 @@ function generate_product_item(){
     const product_item = document.querySelector('.product-items');
     var newproduct_item = `
     <div class="product-content" id="proditem${id_number}">    
-        <select onchange="calcAll();" class="options" id="product${id_number}">
+        <select onchange="calcAll()" class="options" id="product${id_number}">
                    
         </select> 
         <label class="lbl_units" id="unit${id_number}"></label> 
-        <input class="qty" placeholder="кол-во" type="number" id="qty${id_number}">
+        <input onchange="calcAll()" class="qty" placeholder="кол-во" type="number" id="qty${id_number}">
         <label class="lbl_total_cost" id="cost${id_number}"></label>
     </div>
     `
@@ -98,8 +25,9 @@ function generate_product_item(){
     const createOptionsTemplate = item => { 
         return `<option>${item.name}</option>`    
     }
-    const optionItem = document.getElementById(`product${id_number}`);    
+    const optionItem = document.getElementById(`product${id_number}`); 
 
+// productData - массив, находящийся в подключенном файле "productData.js"
     productData.forEach(item => {
         optionItem.innerHTML += createOptionsTemplate(item);
         })
@@ -118,37 +46,47 @@ function remove_product_item(){
 }
 // --------------------------------------------------------------------  
 
+// Вводим переменную для подсчета итоговойстоимости
 let total_price = 0;
 
-const calc_btn = document.querySelector('.calc_btn');
-calc_btn.addEventListener('click', calcAll);
+// const calc_btn = document.querySelector('.calc_btn');
+// calc_btn.addEventListener('click', calcAll);
 
+//--------- Функция, вызывается при изменении значений в select и input-------
 function calcAll(){
-    itemNumber = document.getElementsByClassName('options').length
-    // jsonNumber = productData.length
-    // console.log('всего элементов:' + itemNumber)
+    // определяем кол-во добавленных пользователем элементов
+    itemNumber = document.getElementsByClassName('options').length 
+    // и обходим все эти элементы циклом   
     for (let i = 1; i < itemNumber+1; i++){ 
-        let prod=document.getElementById('product' + i).value;     
-        
+        // Получавем значение из поля продукты
+        let prod=document.getElementById('product' + i).value;
+
+        // ищем это значение в массиве productData и получаем соответствующее ему значение Е.И.
         const units = productData.find(findunits).unit;
         function findunits (item){
             return item.name === prod;
         }
+        //Вставляем полученные Е.И. в лейбл
         document.getElementById('unit' + i).textContent = units;
 
-        const unitprice = productData.find(findunitprice).unitprice;
+         // опять ищем это значение в массиве productData и получаем соответствующее ему значение цены за единицу.    
+        const unitprice = productData.find(findunitprice).unitprice;        
         function findunitprice (item){
             return item.name === prod;
         }
-
+        // Умножаем полученную цену за единицу на кол-во товара, введенное пользователем
         const item_price = unitprice * document.getElementById('qty' + i).value;
+        //Вставляем полученную цену в лейбл
         document.getElementById('cost' + i).textContent = item_price;
 
-        total_price += item_price;        
+        // Складываем значения стоимости у всех добавленных продуктов
+        total_price += item_price;   
+        // Вставляем итоговую стоимость в необходимое "поле" 
         document.querySelector('.total_price').textContent = total_price; 
          
     }
-
+// ---------------------------------------------------------------------------
+// Сбрасываем итоговую стоимость, чтобы не задваивать ее при следующем пересчете
 total_price = 0
 }
 
---
+++ script/productData.js
@@ -0,0 +1,72 @@
+const productData = [
+    {
+        "id": "prod0",
+        "name": "Выберите продукт",
+        "unit": "",
+        "qty": "",
+        "unitprice": ""
+    },
+    {
+        "id": "prod1",
+        "name": "Масло сливочное",
+        "unit": "гр",
+        "qty": "500",
+        "unitprice": "0.19"
+    },
+    {
+        "id": "prod2",
+        "name": "Молоко",
+        "unit": "мл",
+        "qty": "1000",
+        "unitprice": "0.05"
+    },
+    {
+        "id": "prod3",
+        "name": "Мука",
+        "unit": "гр",
+        "qty": "2000",
+        "unitprice": "0.04"
+    },
+    {
+        "id": "prod4",
+        "name": "Разрыхлитель",
+        "unit": "уп",
+        "qty": "1",
+        "unitprice": "12.00"
+    },
+    {
+        "id": "prod5",
+        "name": "Сахар-песок",
+        "unit": "гр",
+        "qty": "5000",
+        "unitprice": "0.05"
+    },
+    {
+        "id": "prod6",
+        "name": "Сахарная пудра",
+        "unit": "гр",
+        "qty": "1000",
+        "unitprice": "0.12"
+    },
+    {
+        "id": "prod7",
+        "name": "Сливки",
+        "unit": "гр",
+        "qty": "1000",
+        "unitprice": "0.28"
+    },
+    {
+        "id": "prod8",
+        "name": "Сыр творожный",
+        "unit": "гр",
+        "qty": "1000",
+        "unitprice": "0.94"
+    },
+    {
+        "id": "prod9",
+        "name": "Яйца куриные",
+        "unit": "шт",
+        "qty": "10",
+        "unitprice": "7.00"
+    }
+]
